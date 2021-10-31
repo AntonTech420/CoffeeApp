c08b5c768409211d2a0f48befc754bf9d7b677b2
AMW <73808213+AntonTech420@users.noreply.github.com>
2021-10-31 13:09:24 +1300

    v2

--- MainTabbedPage.xaml.cs
+++ MainTabbedPage.xaml.cs
@@ -16,5 +16,10 @@ namespace CoffeeApp
         {
             InitializeComponent();
         }
+
+        private async void coffeeButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CoffeeDrinksDetailPage());
+        }
     }
 }
